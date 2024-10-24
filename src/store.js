4d7ed5810d7208c62da0c597808c8f8ecbf129af
Marc <marclawrence88@gmail.com>
2024-10-24 08:48:12 -0400

    added proper initi for project

--- src/store.js
+++ src/store.js
@@ -1,1 +1,10 @@
-//Enter the code here
+import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './CreatSlice';
+import cartReducer from './CreateSlice';
+
+ const store = configureStore({
+    reducer: {
+        cart: cartReducer,
+    },
+});
+export default store
