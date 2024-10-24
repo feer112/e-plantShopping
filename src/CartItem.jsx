ee682818e30a19496446faf3e42b7cd42177e66e
Marc <marclawrence88@gmail.com>
2024-10-24 10:12:50 -0400

    task 3 of final project

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -9,14 +9,22 @@ const CartItem = ({ onContinueShopping }) => {
 
   // Calculate total amount for all products in the cart
   const calculateTotalAmount = () => {
- 
+    let totalCost = 0;
+    cart.forEach((item) => {
+        totalCost += item.cost * item.quantity;
+    });
+
+    return totalCost;
   };
 
   const handleContinueShopping = (e) => {
-   
+    e = CartItem();
+    return e;
   };
 
-
+  const handleCheckoutShopping = (e) => {
+    alert('Functionality to be added for future reference');
+  };
 
   const handleIncrement = (item) => {
   };
--- src/store.js
+++ src/store.js
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from './CreatSlice';
-import cartReducer from './CreateSlice';
+import cartReducer from './CartSlice';
 
- const store = configureStore({
+export default configureStore({
     reducer: {
         cart: cartReducer,
     },
 });
-export default store
